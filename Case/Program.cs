d889e5441d65e9183a6df05f505eecc4bdb54778
ilya1 <ilya1@DESKTOP-R4A653G>
2022-10-21 18:46:23 +0300

    Добавьте файлы проекта.

---
+++ Case/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант 2");
+            Console.WriteLine("Выполнил студент группы 24 ис Козырский Илья");
+            Console.WriteLine("Введите номер месяца: ");
+            string selection = Console.ReadLine();// строковая переменная
+            switch (selection) // сравниваем выражение
+            {
+                case "1": // значение выражения
+                    Console.WriteLine("Январь");
+                    break;
+                case "2": // значение выражения
+                    Console.WriteLine("Февраль");
+                    break;
+                case "3": // значение выражения
+                    Console.WriteLine("Март");
+                    break;
+                case "4": // значение выражения
+                    Console.WriteLine("Апрель");
+                    break;
+                case "5": // значение выражения
+                    Console.WriteLine("Май");
+                    break;
+                case "6": // значение выражения
+                    Console.WriteLine("Июнь");
+                    break;
+                case "7": // значение выражения
+                    Console.WriteLine("Июль");
+                    break;
+                case "8": // значение выражения
+                    Console.WriteLine("Август");
+                    break;
+                case "9": // значение выражения
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case "10": // значение выражения
+                    Console.WriteLine("Октябрь");
+                    break;
+                case "11": // значение выражения
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case "12": // значение выражения
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:// совпадений нет
+                    Console.WriteLine("ошибка при вводе");
+                    break;
+
+
+            }
+            Console.ReadKey();// конец программы
+
+        }
+    }
+}
---
+++ PracticalWork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, y;//объявим переменныые
+            Console.WriteLine("Вариант 2");
+            Console.WriteLine("Выполнил студент группы 24 ис Козырский Илья");
+            Console.WriteLine("Введите a= ");
+            a = Convert.ToDouble(Console.ReadLine()); // Вводим значение a c клавиатуры 
+            Console.WriteLine("Введите b= ");
+            b = Convert.ToDouble(Console.ReadLine()); // Вводим значение b c клавиатуры 
+            if (a > 0) //Условие при положительном или отрицательном значении a
+            {
+                y = a * b; // при условии если a>0, выполняется данное действие 
+                Console.WriteLine($"y={y}"); // вывод на экран значение y              
+            }
+            else
+            {
+                y = a + b; // при условии если a<0, выполняется данное действие 
+                Console.WriteLine($"y={y}"); // вывод на экран значение y
+
+            }
+            Console.ReadKey(); // конец программы
+
+        }
+    }
+}
